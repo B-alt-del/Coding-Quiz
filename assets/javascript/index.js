01d32605852b88a72120debbb3d14a8b093a8f2e
bradschillschool@gmail.com <bradschillschool@gmail.com>
2022-06-30 19:02:32 -0500

    added function for next question: filled the text into the placeholders. added Q&A object.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,15 +2,22 @@
 var quiz_box = document.querySelector('#quiz_box');
 var home_page = document.querySelector('#home_page');
 var home_button = document.querySelector('#start');
+var question = document.querySelector('#question');
+var A = document.querySelector('#A');
+var B = document.querySelector('#B');
+var C = document.querySelector('#C');
+var D = document.querySelector('#D');
+
+
 
 
 quiz_box.style.display = 'none';
 
 
 
-home_button.addEventListener("click", nextPage);
+home_button.addEventListener("click", to_questions);
 
-function nextPage(){
+function to_questions(){
 
     home_page.style.display = 'none';
 
@@ -18,13 +25,27 @@ function nextPage(){
 
     console.log("worked");
 
+    first_question();
+}
+
+function first_question(){
+    question.textContent = q_a.first.question;
+    A.textContent = q_a.first.answer;
+    B.textContent = q_a.first.false[0];
+    C.textContent = q_a.first.false[1];
+    D.textContent = q_a.first.false[2];
+    console.log(q_a);
+}
+
+function second_question(){
+
 }
 
 function evaluate(){
-    
+
 }
 
-var questions_answers ={
+var q_a ={
     first: {
         question: "What is my first name?",
         answer: "Brad",
@@ -44,3 +65,5 @@ var questions_answers ={
         false: ['Chester', 'Barkley', 'Cooper']
     }
 }
+
+
